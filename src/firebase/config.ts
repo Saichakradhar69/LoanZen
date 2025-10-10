a594fdfbead41f1a4ae081e4f458517e6074efb5
sai chakradhar <saichakradhar4@gmail.com>
2025-10-10 03:56:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "loanzen-fbskl",
+  "appId": "1:545957735367:web:df55e3f1ea7a6d531e07e2",
+  "apiKey": "AIzaSyBaOXv_qyC8tH_giz-klvGtZnK5h_eNG0I",
+  "authDomain": "loanzen-fbskl.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "545957735367"
+};
