897e26a9fee3cd67d0325dae228f1851a8547ece
sai chakradhar <saichakradhar4@gmail.com>
2025-10-22 04:14:17 +0000

    error Runtime Error NotAllowedError: Failed to execute 'writeText' on 'C

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useUser, useFirestore, useMemoFirebase } from '@/firebase';
 import { useRouter } from 'next/navigation';
